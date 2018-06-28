54e717156fbeb037a425e03d58cc7bbba97e826c
Lagnesh Thakur <lagnesh.thakur@cybergindia.local>
2018-06-29 01:04:24 +0530

    notes prep

--- src/app/currencies/currencies.component.ts
+++ src/app/currencies/currencies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService } from '../services/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-currencies',
@@ -8,21 +9,34 @@ import { APIService } from '../services/api.service';
 })
 export class CurrenciesCustomComponent implements OnInit {
   public ticker;
+  public currencyName: string;
   public loading: boolean;
+  public notes: string;
 
-  constructor(private apiService: APIService) { }
+  constructor(private apiService: APIService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getSingleTicker(1);
+    this.currencyName = this.activeRoute.snapshot.params['currency'];
+    this.notes = localStorage.getItem(this.currencyName);
+    console.log(this.activeRoute.snapshot.params['currency']);
   }
 
   getSingleTicker(tickerId): void {
-    this.loading = true;
-    debugger
+    // this.loading = true;
     this.apiService.getSingleTicker(tickerId).subscribe(response => {
-      debugger
       this.ticker = JSON.parse(response['_body'].data);
       this.loading = false;
     });
+  }
+
+  onNoteSave(): void {
+    localStorage.setItem(this.currencyName, this.notes);
+    console.log('Note Saved');
+  }
+
+  onNoteClear(): void {
+    localStorage.removeItem(this.currencyName);
+    console.log('Note Cleared');
   }
 }
